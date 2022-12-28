ad8bfa78dcfe1d4e2888dad39f01293947305ab7
agatakazakevic <117447675+agatakazakevic@users.noreply.github.com>
2022-12-28 13:58:31 +0200

    Initial commit

---
+++ memory game 05.21/memory game 05.21/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace memory_game_05._21
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.highScoresToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lblmoves = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.highScoresToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(694, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.lblmoves});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(694, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.newGameToolStripMenuItem.Text = "New game";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // highScoresToolStripMenuItem
+            // 
+            this.highScoresToolStripMenuItem.Name = "highScoresToolStripMenuItem";
+            this.highScoresToolStripMenuItem.Size = new System.Drawing.Size(81, 20);
+            this.highScoresToolStripMenuItem.Text = "High scores";
+            this.highScoresToolStripMenuItem.Click += new System.EventHandler(this.highScoresToolStripMenuItem_Click);
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(42, 17);
+            this.toolStripStatusLabel1.Text = "Moves";
+            // 
+            // lblmoves
+            // 
+            this.lblmoves.Name = "lblmoves";
+            this.lblmoves.Size = new System.Drawing.Size(13, 17);
+            this.lblmoves.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(694, 450);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem highScoresToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel lblmoves;
+    }
+}
+
---
+++ memory game 05.21/memory game 05.21/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace memory_game_05._21
+{
+
+    public partial class Form1 : Form
+    {
+        int moves = 0;
+        string filepath = Application.StartupPath + @"\highscore.txt";//kad nepagalvotu kad tai new lines
+
+
+
+        private void showhighscores()
+        {
+            StreamReader reader = new StreamReader(filepath);
+            MessageBox.Show(reader.ReadToEnd()); 
+            reader.Close();
+
+            reader = new StreamReader(filepath);
+            List<string> allscores = new List<string>();
+            while (!reader.EndOfStream)
+            {
+                allscores.Add(reader.ReadLine());
+            }
+            allscores.Sort();
+            string result = "";
+            int length = 3;
+            if (allscores.Count<3)
+            {
+                length = allscores.Count;
+            }
+            for (int i = 0; i < length; i++)
+            {
+                result += allscores[i] + "\n";
+            }
+            MessageBox.Show("result");
+            reader.Close();
+        }
+
+        private void writehighscore()
+        {
+            FileStream file = new FileStream(filepath, FileMode.Append, FileAccess.Write);
+           
+            var m =Encoding.UTF8.GetBytes( moves.ToString()+" ");
+          file.Write(m,0,m.Length);
+            file.Close();
+
+        }
+        struct imageforgeneration
+        {
+            public Image Image;
+            public bool Placed;
+        
+            public imageforgeneration(Image image, bool placed = false)
+            {
+                Image = image;
+                Placed = placed;
+            }  
+        }
+        Button[,] buttons;
+        imageforgeneration[,] images;
+        int tablesize = 4;
+        List<imageforgeneration> allimages = new List<imageforgeneration>();
+        Random rnd = new Random();
+        int totalopened = 0;
+        int imagesopened = 0;
+        int image1x = -1;
+        int image1y = -1;
+        int image2x = -1;
+        int image2y = -1;
+        int revealtime = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+            generateimages();
+            generatetable();
+        }
+
+        private void generatetable()
+        {
+            Rectangle screenrectangle = RectangleToScreen(ClientRectangle);
+            int height = screenrectangle.Top - Top-menuStrip1.Top;
+            Size = new Size(400+16, 400+height+50);
+            buttons = new Button[tablesize, tablesize];
+            for (int i = 0; i < tablesize; i++)
+            {
+                for (int y = 0; y < tablesize; y++)
+                {
+                    Button btn = new Button
+                    {
+                        Size = new Size(100, 100),
+                        Location = new Point(i * 100, (y * 100)+menuStrip1.Height),
+                        Text = ""
+                    };
+                    buttons[i, y] = btn;
+                    btn.Click += new EventHandler(Button_Click);
+                    Controls.Add(btn);
+                }
+            }
+
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            for (int i = 0; i < tablesize; i++)
+            {
+                for (int j = 0; j < tablesize; j++)
+                {
+                    if (buttons[i, j].Equals(btn))
+                    {
+                        imagesopened++;
+                        switch (imagesopened)
+                        {
+                            case 1:
+                                closeopened();
+                                image1x = i;
+                                image1y = j;
+                                break;
+                            case 2:
+                                image2x = i;
+                                image2y = j;
+                                break;
+
+                        }
+                        buttons[i, j].Click -= Button_Click;
+                        buttons[i, j].Image = images[i,j].Image;
+
+                    }
+                }
+            }
+            if (imagesopened==2)
+            {
+                moves++;
+                lblmoves.Text = moves.ToString();
+                if(images[image1x, image1y].Image== images[image2x, image2y].Image)
+                {
+                    totalopened+= 2;
+                    //check for win
+                    if (totalopened==tablesize*tablesize)
+                    {
+                        MessageBox.Show("YOU WIN");
+                        //check if it hegh score
+                        writehighscore();
+                        moves = 0;
+                        for (int i = 0; i < this.Controls.Count; i++)
+                        {
+                            if (this.Controls[i].GetType()==typeof(Button))
+                            {
+                                this.Controls.Remove(this.Controls[i]);
+                                i--;
+                            }
+                        }
+                        generateimages();
+                        generatetable();
+                       
+
+                    }
+                }
+                else
+                {
+                    revealtime = 3;
+                    timer.Start();
+
+                }
+                imagesopened = 0;
+            }
+        }
+
+        private void generateimages()
+        {
+            var imagesfromrec = Properties.Resources.ResourceManager.GetResourceSet(CultureInfo.CurrentCulture, true, true);
+            foreach (DictionaryEntry entry in imagesfromrec)
+            {
+                allimages.Add(new imageforgeneration((Image)entry.Value));
+            }
+            images = new imageforgeneration[tablesize, tablesize];
+            foreach (imageforgeneration ifg in allimages.OrderBy(x=>rnd.Next()).Take(8))
+            {
+                placetime(ifg);
+                placetime(ifg);
+            }
+        }
+        private void placetime(imageforgeneration imagetoset)
+        {
+            int x;
+            int y;
+            do
+            {
+                x = rnd.Next(0, tablesize);
+                y = rnd.Next(0, tablesize);
+
+
+            } while (images[x, y].Placed == true);
+            images[x, y].Image = imagetoset.Image;
+            images[x, y].Placed = true;
+
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            revealtime--;
+            if (revealtime <= 0)
+            {
+                closeopened();
+            }
+           
+        }
+
+        private void closeopened()
+        {
+            if (image1x>=0 && image1y>=0 && image2x>=0 && image2y>=0)
+            {
+                if (images[image1x, image1y].Image == images[image2x, image2y].Image)
+                    return;
+                else
+                {
+
+                    buttons[image1x, image1y] = null;
+                    buttons[image2x, image2y] = null;
+                    buttons[image1x, image1y].Click += Button_Click;
+                    buttons[image2x, image2y].Click += Button_Click;
+                
+                    image1x = -1;
+                    image1y = -1;
+                    image2x = -1;
+                    image2y = -1;
+                    timer.Stop();
+
+                } 
+            }
+        }
+
+        private void highScoresToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            showhighscores();
+        }
+    }
+}
